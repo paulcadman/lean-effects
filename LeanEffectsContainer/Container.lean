9cbe0b7920781bdddf6806f629bb8bfedf1ce185
Jan Mas Rovira <janmasrovira@gmail.com>
2026-02-12 11:56:55 +0100

    add inject and project

--- LeanEffectsContainer/Container.lean
+++ LeanEffectsContainer/Container.lean
@@ -1,17 +1,19 @@
-structure Container : Type 1 where
-  shape : Type
-  pos : shape → Type
+universe u v w
+
+structure Container : Type (max u v + 1)  where
+  shape : Type u
+  pos : shape → Type v
 
 namespace Container
 
-abbrev extension (C : Container) (A : Type) : Type :=
+abbrev extension (C : Container.{u, v}) (A : Type w) : Type (max u v w) :=
   Σ x : C.shape, C.pos x → A
 
 scoped notation "⟦" C "⟧" => extension C
-scoped notation s "▷" p => Container.mk s p
+scoped notation s " ▷ " p => Container.mk s p
 
-instance (C : Container) : Functor ⟦ C ⟧ where
-  map {α β : Type} (f : α → β) (ca : ⟦C⟧ α) : ⟦C⟧ β :=
+instance (C : Container.{u, v}) : Functor ⟦ C ⟧ where
+  map {α β : Type u} (f : α → β) (ca : ⟦C⟧ α) : ⟦C⟧ β :=
     ⟨ca.fst, f ∘ ca.snd⟩
 
 instance (C : Container) : LawfulFunctor ⟦ C ⟧ where
@@ -29,16 +31,45 @@ def void : Container where
 
 def sum : List Container → Container := List.foldr coproduct void
 
-end Container
-
-open scoped Container
-
-inductive Free (C : Container) (A : Type) : Type where
+inductive Free (C : Container.{u, v}) (A : Type w) : Type (max u v w) where
   | pure : A → Free C A
   | impure : (Σ x : C.shape, C.pos x → Free C A) → Free C A
 
-universe u
-
-inductive Member {A : Type u} (x : A) : List A → Type u where
+class inductive Member {A : Type u} (x : A) : List A → Type u where
   | here {xs} : Member x (x :: xs)
   | there {y xs} : Member x xs → Member x (y :: xs)
+
+export Member (here there)
+
+instance (priority := high) {A : Type u} {x : A} {xs} : Member x (x :: xs) := .here
+instance (priority := low) {A : Type u} {y x : A} {xs} [m : Member x xs] : Member x (y :: xs) := .there m
+
+notation (priority := high) x " ∈ " xs:50 => Member x xs
+
+section
+
+variable
+  {C : Container}
+  {A : Type u}
+
+def inject {ops : List Container} : C ∈ ops → ⟦C⟧ A → ⟦sum ops⟧ A
+  | here, ⟨s, pf⟩ => ⟨.inl s , pf⟩
+  | there m, prog =>
+      match inject m prog with
+      | ⟨s, p⟩ => by
+            -- ⟨.inr s, p⟩ is enough, but for pedagogical reasons we keep the step by step definition
+            refine ⟨.inr s, ?_⟩
+            unfold sum; simp only [List.foldr]
+            unfold coproduct; simp only [Sum.elim]
+            unfold sum coproduct at p
+            exact p
+
+def project {ops : List Container} : C ∈ ops → ⟦sum ops⟧ A → Option (⟦C⟧ A)
+ | here, ⟨.inl s, pf⟩ => some ⟨s, pf⟩
+ | here, ⟨.inr _, _⟩ => none
+ | there _, ⟨.inl _, _⟩ => none
+ | there p, ⟨.inr s, pf⟩ => project p ⟨s, pf⟩
+
+end
+
+end Container
