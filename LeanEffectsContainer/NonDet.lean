f473e20f7ce58afab30aac55e3d09bb9a5b9e230
Paul Cadman <git@paulcadman.dev>
2026-03-01 16:37:00 +0000

    Add msplit example

--- LeanEffectsContainer/NonDet.lean
+++ LeanEffectsContainer/NonDet.lean
@@ -1,4 +1,5 @@
 import LeanEffectsContainer.Prog
+import LeanEffectsContainer.State
 
 open scoped Container
 
@@ -24,32 +25,65 @@ def NonDet : Effect where
   ops := NonDetOpsC
   scps := NonDetScpsC
 
+def lower2
+    {effs : List Effect} {α : Type} {n : Nat} :
+    ProgN effs α (n + 2) → ProgN effs α (n + 1)
+  | .varS x => x
+  | .op c k => .op c (fun p => lower2 (k p))
+  | .scp c k => .scp c (fun p => lower2 (n := n + 1) (k p))
+termination_by x => sizeOf x
+decreasing_by
+  all_goals sorry
+
 namespace NonDet
 
-section
+variable
+  {effs : List Effect}
+  {α : Type}
 
-universe u
+section SmartConstructor
 
 variable
-{effs : List Effect}
-[NonDet ∈ effs]
+  [m : NonDet ∈ effs]
 
 def choiceBool : Prog effs Bool :=
-  opEff (e:=NonDet) ⟨.choiceOp, fun b => Prog.var b⟩
+  opEff (e:=NonDet) ⟨NonDetOps.choiceOp, fun b => Prog.var b⟩
 
-def choice {α : Type} (p : Prog effs α) (q : Prog effs α) : Prog effs α := do
-  let b ← choiceBool
+def choice (p : Prog effs α) (q : Prog effs α) : Prog effs α := do
+  let b : Bool ← choiceBool
   if b then p else q
 
 scoped notation p " ?? " q => choice p q
 
-def fail {α : Type} : Prog effs α :=
-  opEff (e:=NonDet) ⟨.failOp, fun e => nomatch e⟩
+def fail : Prog effs α :=
+  opEff (e:=NonDet) ⟨NonDetOps.failOp, fun e => nomatch e⟩
 
-def once {α : Type} (p : Prog effs α) : Prog effs α :=
-  scpEff (e:=NonDet) ⟨.once, fun _ => ProgN.varS p⟩
+def once (p : Prog effs α) : Prog effs α :=
+  scpEff (e:=NonDet) ⟨NonDetScps.once, fun _ => ProgN.varS p⟩
 
-def runList {α : Type u} :
+partial def msplit : Prog effs α → Prog effs (Option (α × Prog effs α))
+  | ProgN.varS (ProgN.var0 x) => pure (some (x, fail))
+  | ProgN.op c k =>
+      match Container.project (opsMem m) ⟨c, k⟩ with
+      | some ⟨.choiceOp, k'⟩ => do
+          let l ← msplit (k' true)
+          match l with
+          | some (x, rest) => pure (some (x, rest ?? k' false))
+          | none => msplit (k' false)
+      | some ⟨.failOp, _⟩ => pure none
+      | none => Prog.op ⟨c, fun p => msplit (k p)⟩
+  | ProgN.scp c k =>
+      match Container.project (scpsMem m) ⟨c, k⟩ with
+      | some ⟨.once, k'⟩ => do
+          let r ← msplit (lower2 (k' PUnit.unit))
+          match r with
+          | some (x, _) => pure (some (x, fail))
+          | none => pure none
+      | none => Prog.scp ⟨c, fun p => ProgN.varS (msplit (lower2 (k p)))⟩
+
+end SmartConstructor
+
+def runList {α : Type} :
   Prog (NonDet :: effs) α → Prog effs (List α) :=
   Prog.foldP
     (P := fun _ => Prog effs (List α))
@@ -72,7 +106,7 @@ def runList {α : Type u} :
         | x :: _ => pure [x]
       | .inr s => Prog.scp ⟨s, fun p => ProgN.varS (k p)⟩)
 
-def runFirst {effs : List Effect} {α : Type u} :
+def runFirst :
     Prog (NonDet :: effs) α → Prog effs (Option α) :=
   Prog.foldP
     (P := fun _ => Prog effs (Option α))
@@ -91,7 +125,6 @@ def runFirst {effs : List Effect} {α : Type u} :
       match c with
       | .inl .once => k PUnit.unit
       | .inr s => Prog.scp ⟨s, (fun p => ProgN.varS (k p))⟩)
-end
 
 end NonDet
 
@@ -116,5 +149,68 @@ def exOnce : Prog [NonDet] Nat :=
 
 #guard Prog.run (runList exOnce) = [1]
 #guard Prog.run (runFirst exOnce) = .some 1
+
+def exSplitNone : Prog [NonDet] (Option (Nat × Prog [NonDet] Nat)) :=
+  msplit fail
+
+#guard
+  match Prog.run (runFirst exSplitNone) with
+  | some none => true
+  | _ => false
+
+def exSplitOne : Prog [NonDet] (Option (Nat × Prog [NonDet] Nat)) :=
+  msplit (pure 7)
+
+#guard
+  match Prog.run (runFirst exSplitOne) with
+  | some (some (7, rest)) => Prog.run (runList rest) = []
+  | _ => false
+
+def exSplitBoth : Prog [NonDet] (Option (Nat × Prog [NonDet] Nat)) :=
+  msplit (pure 1 ?? pure 2 ?? fail ?? pure 3)
+
+#guard
+  match Prog.run (runFirst exSplitBoth) with
+  | some (some (1, rest)) => Prog.run (runList rest) = [2, 3]
+  | _ => false
+
+def exSplitOnce : Prog [NonDet] (Option (Nat × Prog [NonDet] Nat)) :=
+  msplit (once (pure 1 ?? pure 2))
+
+#guard
+  match Prog.run (runFirst exSplitOnce) with
+  | some (some (1, rest)) => Prog.run (runList rest) = []
+  | _ => false
+
+def exStateNonDet : Prog [NonDet, State Nat] Nat := do
+  let n ← State.get
+  NonDet.choice
+    (pure n)
+    (do
+      State.put (n + 1)
+      State.get)
+
+#guard Prog.run (State.run 0 (NonDet.runList exStateNonDet)) = (1, [0, 1])
+
+def exStateNonDetSplit : Prog [NonDet, State Nat] (Option (Nat × Prog [NonDet, State Nat] Nat)) :=
+  msplit exStateNonDet
+
+#guard
+  match Prog.run (State.run 0 (runFirst exStateNonDetSplit)) with
+  | (0, some (some (0, rest))) => Prog.run (State.run 0 (NonDet.runList rest)) = (1, [1])
+  | _ => false
+
+def exSplitNonHead : Prog [State Nat, NonDet] (Option (Nat × Prog [State Nat, NonDet] Nat)) := do
+  let n ← State.get
+  msplit (NonDet.choice
+    (pure n)
+    (do
+      State.put (n + 1)
+      State.get))
+
+#guard
+  match Prog.run (NonDet.runFirst (effs := []) (State.run 0 exSplitNonHead)) with
+  | some (0, some (0, _)) => true
+  | _ => false
 
 end Examples
